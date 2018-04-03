19280d7d91f8650435e9f060325b55f1a8027dfc
Gabriel Machado <g_machado1@hotmail.com>
2018-04-03 09:50:52 -0300

    Estilizado autor.xhtml com primefaces e implementado editar e remover

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,15 +1,26 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
-public class AutorBean {
+@ViewScoped
+public class AutorBean implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private Autor autor = new Autor();
 	private Integer autorId;
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
 
 	public Integer getAutorId() {
 		return autorId;
@@ -26,14 +37,26 @@ public class AutorBean {
 	public Autor getAutor() {
 		return autor;
 	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void remover(Autor autor) {
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
 
 	public String gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		if (this.autor.getId() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
 
 		this.autor = new Autor();
 		
-		return "livro?faces-redirect=true";
+		return "autor?faces-redirect=true";
 	}
 }
